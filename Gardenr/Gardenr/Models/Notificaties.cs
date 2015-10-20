68d18a6ba7640e8cd83c0e7584d89ac89f4b1095
cedriclecat <cedric.lecat@student.howest.be>
2015-10-20 16:58:31 +0200

    fuck github

---
+++ Gardenr/Gardenr/Models/Notificaties.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gardenr.Models
+{
+    class Notificaties
+    {
+        public int ID { get; set; }
+        public int PlantID { get; set; }       
+        public int GebruikerID { get; set; }
+        public string Omschrijving { get; set; }
+        public DateTime Datum { get; set; }
+
+    }
+}
---
+++ Gardenr/Gardenr/Models/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gardenr.Models
+{
+    class Plant
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public string Description { get; set; }
+        public string FotoUrl { get; set; }
+        public DateTime ZaaiBegin { get; set; }
+        public DateTime ZaaiEind { get; set;}
+        public DateTime OogstBegin { get; set; }
+        public DateTime Oogstbegin { get; set; }
+    }
+}
