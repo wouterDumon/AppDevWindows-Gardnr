025e60c7dc6471d6c7ca3afd02c97a09a85d40ae
cedriclecat <cedric.lecat@student.howest.be>
2015-10-23 13:48:02 +0200

    message aanpassen voor plant mee te sturen met vieuw

--- Gardenr/Gardenr/Models/Plant.cs
+++ Gardenr/Gardenr/Models/Plant.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Gardenr.Models
 {
-    class Plant
+    public class Plant
     {
         public int ID { get; set; }
         public string Naam { get; set; }
