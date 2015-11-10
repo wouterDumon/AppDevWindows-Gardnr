eff59cf77e089b69c15713101dc1b3812e410658
cedriclecat <cedric.lecat@student.howest.be>
2015-11-11 00:38:41 +0100

    test met 1 repo voor alles

---
+++ Gardenr/Gardenr/Repositories/DataRepository.cs
@@ -0,0 +1,36 @@
+﻿using Gardenr.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace Gardenr.Repositories
+{
+    class DataRepository
+    {
+       
+      public IEnumerable GetItems(Type deklasse)
+      {
+            var name = deklasse.Name;
+                        
+            return pat;
+      }
+     
+    private static ObservableCollection<T> CreateObservable(IEnumerable<T> enumerable)
+        {
+            return new ObservableCollection<T>(enumerable);
+        }
+
+
+
+
+
+    
+    }
+}
