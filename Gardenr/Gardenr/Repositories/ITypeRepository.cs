b473a019504c30b8be2c58a9386d8f28e79ec499
cedriclecat <cedric.lecat@student.howest.be>
2015-11-11 18:27:44 +0100

    types repo
    niet getest

---
+++ Gardenr/Gardenr/Repositories/ITypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Gardenr.Models;
+
+namespace Gardenr.Repositories
+{
+    interface ITypeRepository
+    {
+        void AddType(TypeC nitem);
+        void AdjustType(TypeC nitem);
+        void DeleteType(TypeC nitem);
+        Task<TypeC> GetType(string nitem);
+        Task<ObservableCollection<TypeC>> GetTypes();
+    }
+}
--- Gardenr/Gardenr/Repositories/TypeRepository.cs
+++ Gardenr/Gardenr/Repositories/TypeRepository.cs
@@ -1,25 +1,137 @@
 ﻿using Gardenr.Models;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Popups;
 
 namespace Gardenr.Repositories
 {
-    class TypeRepository
+    class TypeRepository : ITypeRepository
     {
-        public List<TypeC> GetTypes()
+        private IMobileServiceSyncTable<TypeC> Table =
+                App.MobileService.GetSyncTable<TypeC>(); // offline sync
+        private async Task InitLocalStoreAsync()
         {
-            List<TypeC> typ = new List<TypeC>();
+            if (!App.MobileService.SyncContext.IsInitialized)
+            {
 
-            return typ;
+                await App.MobileService.SyncContext.InitializeAsync(App.store);
+            }
+
+
+
+            await SyncAsync();
         }
-        public TypeC GetTypeById(int id)
-        {
-            TypeC typ = new TypeC();
 
-            return typ;
+        private async Task SyncAsync()
+        {
+            String errorString = null;
+
+            try
+            {
+                await App.MobileService.SyncContext.PushAsync();
+                // first param is query ID, used for incremental sync
+                await Table.PullAsync("TypeCs", Table.CreateQuery());
+            }
+
+            catch (MobileServicePushFailedException ex)
+            {
+                errorString = "Push failed because of sync errors. " +
+                              "You may be offine.\nMessage: " +
+                              ex.Message + "\nPushResult.Status: " +
+                              ex.PushResult.Status.ToString();
+            }
+            catch (Exception ex)
+            {
+                errorString = "Pull failed: " + ex.Message +
+                  "\n\nIf you are still in an offline scenario, " +
+                  "you can try your Pull again when connected with " +
+                  "your Mobile Service.";
+            }
+
+            if (errorString != null)
+            {
+                MessageDialog d = new MessageDialog(errorString);
+                await d.ShowAsync();
+            }
+        }
+        private MobileServiceCollection<TypeC, TypeC> items;
+        private async Task RefreshItems()
+        {
+            MobileServiceInvalidOperationException exception = null;
+            try
+            {
+                // This code refreshes the entries in the list view by querying the TodoItems table.
+                // The query excludes completed TodoItems
+                items = await Table.ToCollectionAsync();
+            }
+            catch (MobileServiceInvalidOperationException e)
+            {
+                exception = e;
+            }
+
+            if (exception != null)
+            {
+                await new MessageDialog(exception.Message, "Error loading items").ShowAsync();
+            }
+            else
+            {
+                //    ListItems.ItemsSource = items;
+                //  this.ButtonSave.IsEnabled = true;
+            }
+        }
+
+        public async Task<ObservableCollection<TypeC>> GetTypes()
+        {
+            await InitLocalStoreAsync();
+            await RefreshItems();
+            ObservableCollection<TypeC> ni = new ObservableCollection<TypeC>();
+
+            foreach (TypeC types in items)
+            {
+                ni.Add(types);
+            }
+            return ni;
+        }
+        public async Task<TypeC> GetType(string nitem)
+        {
+            await InitLocalStoreAsync();
+            await RefreshItems();
+            foreach (TypeC ni in items)
+            {
+                if (ni.ID == nitem)
+                {
+                    return ni;
+                }
+            }
+            return null;
+        }
+
+        public async void AdjustType(TypeC nitem)
+        {
+            await InitLocalStoreAsync();
+            await Table.UpdateAsync(nitem);
+            await SyncAsync();
+            await RefreshItems();
+        }
+        public async void AddType(TypeC nitem)
+        {
+            await InitLocalStoreAsync();
+            await Table.InsertAsync(nitem);
+            await SyncAsync();
+            await RefreshItems();
+        }
+        public async void DeleteType(TypeC nitem)
+        {
+            await InitLocalStoreAsync();
+            await Table.DeleteAsync(nitem);
+            await SyncAsync();
+            await RefreshItems();
         }
     }
 }
