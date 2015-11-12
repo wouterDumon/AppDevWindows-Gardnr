a0087c98aae0766339b80b04b0c4e32ad5a9916a
cedriclecat <cedric.lecat@student.howest.be>
2015-11-12 20:19:48 +0100

    instellingen2
    instellingen model waarbij taal opgehaald word met een id

---
+++ Gardenr/Gardenr/Models/Instellingen2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gardenr.Models
+{
+    class Instellingen2
+    {
+        public string ID { get; set; }
+        public string TaalID { get; set; }
+        public bool Cortana { get; set; }
+        public bool PushNotificaties { get; set; }
+    }
+}
