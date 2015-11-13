99e51682502421218a9435e1c7221ca7ba217aad
wouterDumon <wouter.dumon@student.howest.be>
2015-11-13 12:07:46 +0100

    github KSDFJQSKLDFJKLQSDFJMKLQSDFLMKQSJDFMKLJQ

--- Gardenr/Gardenr/App.xaml.cs
+++ Gardenr/Gardenr/App.xaml.cs
@@ -33,7 +33,7 @@ namespace Gardenr
         internal static string FacebookId = String.Empty;
         internal static Gebruiker Gebruiker = null;
         public static bool isAuthenticated = false;
-        public static MobileServiceSQLiteStore store = new MobileServiceSQLiteStore("localstore5.db");
+        public static MobileServiceSQLiteStore store = new MobileServiceSQLiteStore("localstore6.db");
         internal static Frame frame;
            
 
