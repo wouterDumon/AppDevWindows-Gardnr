7ba05d231a59cb322e46bcce3dad381afe978b16
wouterDumon <wouter.dumon@student.howest.be>
2015-12-28 19:26:27 +0100

    Profiel adaptive

--- Gardenr/Gardenr/Views/Profiel_Huidig.xaml.cs
+++ Gardenr/Gardenr/Views/Profiel_Huidig.xaml.cs
@@ -37,6 +37,18 @@ namespace Gardenr.Views
             a.GoToTuinObject.Execute("iets");
 
         }
+        private void Position_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ProfielVM a = this.DataContext as ProfielVM;
+            FrameworkElement parent = (FrameworkElement)((Button)sender).Parent;
+            string parent_name = parent.Tag.ToString();
+
+
+
+            a.UnfavoriteM(parent_name);
+
+
+        }
 
         private void StackPanel_Loaded(object sender, RoutedEventArgs e)
         {
@@ -102,6 +114,198 @@ namespace Gardenr.Views
             }
             mn.Width = (ListItems.ActualWidth / berekenhoeveelitems()) - 15;
             mn.Height = (ListItems.ActualWidth / berekenhoeveelitems()) - 15;
+        }
+
+        /*
+        VOOR DESKTOP   
+        */
+
+        private List<StackPanel> gridsFAV = new List<StackPanel>();
+        private List<Ellipse> gridselFAV = new List<Ellipse>();
+
+        private void ListItemsDESK_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (gridsFAV.Count != 0)
+            {
+                foreach (StackPanel mn in gridsFAV)
+                {
+                    mn.Height = ListItemsDESK.ActualHeight - 15;
+                }
+            }
+            if (gridselFAV.Count != 0)
+            {
+                foreach (Ellipse mn in gridselFAV)
+                {
+                    mn.Width = ListItemsDESK.ActualHeight - 95;
+                    mn.Height = ListItemsDESK.ActualHeight - 95;
+                }
+            }
+        }
+
+        private double berekenhoeveelitemsFAV(double a)
+        {
+            if (a > 1300) return 8;
+            else
+           if (a > 1150) return 7;
+            else if (a > 1000)
+            {
+                return 6;
+            }
+            else if (a > 850) return 5;
+            else if (a > 700)
+            {
+                return 4;
+            }
+            else {
+                return 3;
+            }
+
+        }
+
+        private void StackPanel_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+            StackPanel mn = sender as StackPanel;
+
+            if (gridsFAV.Contains(mn)) { }
+            else {
+                gridsFAV.Add(mn);
+            }
+            mn.Height = ListItemsDESK.ActualHeight - 15;
+
+        }
+
+        private void Ellipse_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Ellipse mn = sender as Ellipse;
+
+            if (gridselFAV.Contains(mn)) { }
+            else {
+                gridselFAV.Add(mn);
+            }
+            mn.Width = ListItemsDESK.ActualHeight - 95;
+            mn.Height = ListItemsDESK.ActualHeight - 95;
+        }
+        private void ListItemsDESK_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            ProfielVM a = this.DataContext as ProfielVM;
+            a.GoToPlant.Execute("iets");
+        }
+        //END FAV
+        //BEGINHISTORIEK
+
+        private List<StackPanel> gridsHIST = new List<StackPanel>();
+        private List<Ellipse> gridselHIST = new List<Ellipse>();
+
+        private void ListItemsHIST_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (gridsHIST.Count != 0)
+            {
+                foreach (StackPanel mn in gridsHIST)
+                {
+                    mn.Height = ListItemsHIST.ActualHeight - 15;
+                }
+            }
+            if (gridselHIST.Count != 0)
+            {
+                foreach (Ellipse mn in gridselHIST)
+                {
+                    mn.Width = ListItemsHIST.ActualHeight - 95;
+                    mn.Height = ListItemsHIST.ActualHeight - 95;
+                }
+            }
+        }
+
+        private void StackPanel_Loaded_2(object sender, RoutedEventArgs e)
+        {
+            StackPanel mn = sender as StackPanel;
+
+            if (gridsHIST.Contains(mn)) { }
+            else {
+                gridsHIST.Add(mn);
+            }
+            mn.Height = ListItemsHIST.ActualHeight - 15;
+        }
+
+        private void Ellipse_Loaded_2(object sender, RoutedEventArgs e)
+        {
+            Ellipse mn = sender as Ellipse;
+
+            if (gridselHIST.Contains(mn)) { }
+            else {
+                gridselHIST.Add(mn);
+            }
+            mn.Width = ListItemsHIST.ActualHeight - 95;
+            mn.Height = ListItemsHIST.ActualHeight - 95;
+
+        }
+
+        private void ListItemsHIST_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ProfielVM a = this.DataContext as ProfielVM;
+            a.GoToTuinObject.Execute("iets");
+        }
+        //HUIDIG
+        private List<StackPanel> gridsHuidig = new List<StackPanel>();
+        private List<Ellipse> gridselHuidig = new List<Ellipse>();
+
+        private void ListItemsHUID_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (gridsHuidig.Count != 0)
+            {
+                foreach (StackPanel mn in gridsHuidig)
+                {
+                    mn.Height = ListItemsHUID.ActualHeight - 15;
+                }
+            }
+            if (gridselHuidig.Count != 0)
+            {
+                foreach (Ellipse mn in gridselHuidig)
+                {
+                    mn.Width = ListItemsHUID.ActualHeight - 95;
+                    mn.Height = ListItemsHUID.ActualHeight - 95;
+                    //    mn.Height = (ListItemsHUID.ActualWidth / berekenhoeveelitemsFAV(ListItemsHUID.ActualWidth)) - 15;
+                }
+            }
+        }
+
+        private void ListItemsHUID_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ProfielVM a = this.DataContext as ProfielVM;
+            a.GoToTuinObject.Execute("iets");
+
+        }
+
+        private void StackPanel_Loaded_3(object sender, RoutedEventArgs e)
+        {
+
+            StackPanel mn = sender as StackPanel;
+
+            if (gridsHuidig.Contains(mn)) { }
+            else {
+                gridsHuidig.Add(mn);
+            }
+            mn.Height = ListItemsHUID.ActualHeight - 15;
+
+        }
+
+        private void Ellipse_Loaded_3(object sender, RoutedEventArgs e)
+        {
+
+            Ellipse mn = sender as Ellipse;
+
+            if (gridselHuidig.Contains(mn)) { }
+            else {
+                gridselHuidig.Add(mn);
+            }
+            mn.Width = ListItemsHUID.ActualHeight - 95;
+            mn.Height = ListItemsHUID.ActualHeight - 95;
+
+            //mn.Height = (ListItemsHUID.ActualWidth / berekenhoeveelitemsFAV(ListItemsHUID.ActualWidth)) - 15;
         }
     }
 }
